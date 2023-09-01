b985cd5f6f549597dd2e09e859104fa332c3dc95
Ankur Halder <ankur.halder12345@gmail.com>
2023-09-01 20:24:25 +0530

    sivenavbar update

--- container/sidenavbar/SideNavbar.jsx
+++ container/sidenavbar/SideNavbar.jsx
@@ -3,7 +3,12 @@
 import React, { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+	faBars,
+	faXmark,
+	faMagnifyingGlass,
+} from "@fortawesome/free-solid-svg-icons";
 const SideNavbar = () => {
 	const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
@@ -14,22 +19,29 @@ const SideNavbar = () => {
 	return (
 		<div className={`sidebar ${isSidebarOpen ? "open" : ""}`}>
 			<div className="logo-details">
-				<i className="bx bxl-c-plus-plus icon"></i>
+				<Image src="/SpaceOtaku.png" width={50} height={50} alt="logo"></Image>
 				<div className="logo_name">CodingLab</div>
-				<i
-					className={`bx ${isSidebarOpen ? "bx-menu-alt-right" : "bx-menu"}`}
-					onClick={toggleSidebar}
-				></i>
+				{isSidebarOpen ? (
+					<FontAwesomeIcon
+						icon={faBars}
+						onClick={toggleSidebar}
+					></FontAwesomeIcon>
+				) : (
+					<FontAwesomeIcon
+						icon={faXmark}
+						onClick={toggleSidebar}
+					></FontAwesomeIcon>
+				)}
 			</div>
 			<ul className="nav-list">
 				<li>
-					<i className="bx bx-search"></i>
+					<FontAwesomeIcon icon={faMagnifyingGlass}></FontAwesomeIcon>
 					<input type="text" placeholder="Search..." />
 					<span className="tooltip">Search</span>
 				</li>
 				<li>
 					<Link href="#">
-						<i className="bx bx-grid-alt"></i>
+						<FontAwesomeIcon icon={faMagnifyingGlass}></FontAwesomeIcon>
 						<span className="links_name">Dashboard</span>
 					</Link>
 					<span className="tooltip">Dashboard</span>
@@ -37,7 +49,7 @@ const SideNavbar = () => {
 
 				<li>
 					<Link href="#">
-						<i className="bx bx-user"></i>
+						<FontAwesomeIcon icon={faMagnifyingGlass}></FontAwesomeIcon>
 						<span className="links_name">User</span>
 					</Link>
 					<span className="tooltip">User</span>
@@ -45,13 +57,18 @@ const SideNavbar = () => {
 
 				<li className="profile">
 					<div className="profile-details">
-						<Image src="/profile.jpg" alt="profileImg" width={50} height={50} />
+						<Image
+							src="/SpaceOtaku.png"
+							width={50}
+							height={50}
+							alt="logo"
+						></Image>
 						<div className="name_job">
 							<div className="name">Prem Shahi</div>
 							<div className="job">Web designer</div>
 						</div>
 					</div>
-					<i className="bx bx-log-out" id="log_out"></i>
+					<FontAwesomeIcon icon={faMagnifyingGlass}></FontAwesomeIcon>
 				</li>
 			</ul>
 		</div>
