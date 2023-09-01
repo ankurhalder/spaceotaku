9717da4cb20a16d3751dfacdd58ae1f8b8571f94
Ankur Halder <ankur.halder12345@gmail.com>
2023-09-01 19:28:08 +0530

    yarn added

--- container/sidenavbar/SideNavbar.jsx
+++ container/sidenavbar/SideNavbar.jsx
@@ -1,5 +1,61 @@
-function SideNavbar() {
-	return <div></div>;
-}
+// components/SideNavbar.js
+
+import React, { useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+const SideNavbar = () => {
+	const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+	const toggleSidebar = () => {
+		setIsSidebarOpen(!isSidebarOpen);
+	};
+
+	return (
+		<div className={`sidebar ${isSidebarOpen ? "open" : ""}`}>
+			<div className="logo-details">
+				<i className="bx bxl-c-plus-plus icon"></i>
+				<div className="logo_name">CodingLab</div>
+				<i
+					className={`bx ${isSidebarOpen ? "bx-menu-alt-right" : "bx-menu"}`}
+					onClick={toggleSidebar}
+				></i>
+			</div>
+			<ul className="nav-list">
+				<li>
+					<i className="bx bx-search"></i>
+					<input type="text" placeholder="Search..." />
+					<span className="tooltip">Search</span>
+				</li>
+				<li>
+					<Link href="#">
+						<i className="bx bx-grid-alt"></i>
+						<span className="links_name">Dashboard</span>
+					</Link>
+					<span className="tooltip">Dashboard</span>
+				</li>
+
+				<li>
+					<Link href="#">
+						<i className="bx bx-user"></i>
+						<span className="links_name">User</span>
+					</Link>
+					<span className="tooltip">User</span>
+				</li>
+
+				<li className="profile">
+					<div className="profile-details">
+						<Image src="/profile.jpg" alt="profileImg" width={50} height={50} />
+						<div className="name_job">
+							<div className="name">Prem Shahi</div>
+							<div className="job">Web designer</div>
+						</div>
+					</div>
+					<i className="bx bx-log-out" id="log_out"></i>
+				</li>
+			</ul>
+		</div>
+	);
+};
 
 export default SideNavbar;
