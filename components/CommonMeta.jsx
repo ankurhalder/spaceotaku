3eba54ab490b24fe3e5bf35eb84305df32dae0e2
Ankur Halder <ankur.halder12345@gmail.com>
2023-09-02 01:55:10 +0530

    removed

--- components/CommonMeta.jsx
+++ components/CommonMeta.jsx
@@ -62,25 +62,6 @@ export default function CommonMeta() {
 
 			{/* Yandex Webmaster Verification */}
 			<meta name="yandex-verification" content="6bf6f827d7e4d463" />
-			{/* Yandex.Metrika counter code */}
-			<script
-				type="text/javascript"
-				dangerouslySetInnerHTML={{
-					__html: `
-        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-        m[i].l=1*new Date();
-        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-        ym(94814683, "init", {
-             clickmap:true,
-             trackLinks:true,
-             accurateTrackBounce:true,
-             webvisor:true
-        });
-      `,
-				}}
-			/>
 		</Head>
 	);
 }
