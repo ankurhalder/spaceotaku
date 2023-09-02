c506b40dd1e1cbe095a07cdfcf369be724e72696
Ankur Halder <ankur.halder12345@gmail.com>
2023-09-02 18:32:53 +0530

    solar system added

---
+++ pages/solar-system/index.jsx
@@ -0,0 +1,11 @@
+export default function SolarSystem() {
+	return (
+		<div>
+			<iframe
+				className="solar-system-container"
+				src="https://spaceotakusolarsystem.netlify.app/"
+				title="Space Otaku Solar System"
+			></iframe>
+		</div>
+	);
+}
