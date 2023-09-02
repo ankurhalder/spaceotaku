692c1131ba3850c8020e3ca7d55b84480370485f
Ankur Halder <ankur.halder12345@gmail.com>
2023-09-02 19:34:51 +0530

    new navbar successfully implemented

--- container/index.jsx
+++ container/index.jsx
@@ -1,1 +1,2 @@
 export { default as SideNavbar } from "./sidenavbar/SideNavbar";
+export { default as SideBar } from "./sidebar/SideBar";
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,13 +1,13 @@
 import "../styles/main.scss";
 import { CustomHead, CommonMeta } from "@/components";
 import { Fragment } from "react";
-// import { SideNavbar } from "@/container";
+import { SideBar } from "@/container";
 export default function App({ Component, pageProps }) {
 	return (
 		<Fragment>
 			<CustomHead></CustomHead>
 			<CommonMeta></CommonMeta>
-			{/* <SideNavbar></SideNavbar> */}
+			<SideBar></SideBar>
 			<Component {...pageProps} />
 		</Fragment>
 	);
