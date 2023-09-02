e84b013baf2dce3435d8b2cdd2592bf506831da9
Ankur Halder <ankur.halder12345@gmail.com>
2023-09-02 18:37:37 +0530

    sidenavbar removed

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,13 +1,13 @@
 import "../styles/main.scss";
 import { CustomHead, CommonMeta } from "@/components";
 import { Fragment } from "react";
-import { SideNavbar } from "@/container";
+// import { SideNavbar } from "@/container";
 export default function App({ Component, pageProps }) {
 	return (
 		<Fragment>
 			<CustomHead></CustomHead>
 			<CommonMeta></CommonMeta>
-			<SideNavbar></SideNavbar>
+			{/* <SideNavbar></SideNavbar> */}
 			<Component {...pageProps} />
 		</Fragment>
 	);
