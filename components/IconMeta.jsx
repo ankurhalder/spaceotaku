fa2f6e5aa0087f009c7e55bb5ec23093ceeb9344
Ankur Halder <ankur.halder12345@gmail.com>
2023-09-03 22:03:21 +0530

    all icons added successfully

--- components/IconMeta.jsx
+++ components/IconMeta.jsx
@@ -3,27 +3,91 @@ import Head from "next/head";
 export default function IconMeta() {
 	return (
 		<Head>
-			{/* <link rel="icon" type="image/x-icon" href="/favicon_icons/favicon.ico" />
+			<link rel="icon" type="image/x-icon" href="/favicon.ico" />
 			<link
 				rel="icon"
 				type="image/png"
-				href="/favicon_icons/favicon-32x32.png"
+				href="/favicon-32x32.png"
 				sizes="32x32"
 			></link>
 			<link
 				rel="icon"
 				type="image/png"
-				href="/favicon_icons/favicon-16x16.png"
+				href="/favicon-16x16.png"
 				sizes="16x16"
 			></link>
 			<link
 				rel="icon"
 				type="image/png"
 				sizes="96x96"
-				href="/favicon_icons/favicon-96x96.png"
-			/> */}
-			{/* <link rel="manifest" href="/manifest.json" /> */}
-			{/* <meta name="msapplication-config" content="/browserconfig.xml" /> */}
+				href="/favicon-96x96.png"
+			/>
+			<link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png" />
+			<link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png" />
+			<link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png" />
+			<link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png" />
+			<link
+				rel="apple-touch-icon"
+				sizes="114x114"
+				href="/apple-icon-114x114.png"
+			/>
+			<link
+				rel="apple-touch-icon"
+				sizes="120x120"
+				href="/apple-icon-120x120.png"
+			/>
+			<link
+				rel="apple-touch-icon"
+				sizes="144x144"
+				href="/apple-icon-144x144.png"
+			/>
+			<link
+				rel="apple-touch-icon"
+				sizes="152x152"
+				href="/apple-icon-152x152.png"
+			/>
+			<link
+				rel="apple-touch-icon"
+				sizes="180x180"
+				href="/apple-icon-180x180.png"
+			/>
+			<link rel="android-icon" sizes="36x36" href="/android-icon-36x36.png" />
+			<link rel="android-icon" sizes="48x48" href="/android-icon-48x48.png" />
+			<link rel="android-icon" sizes="72x72" href="/android-icon-72x72.png" />
+			<link rel="android-icon" sizes="96x96" href="/android-icon-96x96.png" />
+			<link
+				rel="android-icon"
+				sizes="144x144"
+				href="/android-icon-144x144.png"
+			/>
+
+			<link rel="icon" sizes="192x192" href="/android-icon-192x192.png" />
+			<link
+				rel="icon"
+				type="image/png"
+				sizes="32x32"
+				href="/favicon-32x32.png"
+			/>
+			<link
+				rel="icon"
+				type="image/png"
+				sizes="96x96"
+				href="/favicon-96x96.png"
+			/>
+			<link
+				rel="icon"
+				type="image/png"
+				sizes="16x16"
+				href="/favicon-16x16.png"
+			/>
+			<link rel="ms-icon" sizes="70x70" href="/ms-icon-70x70.png" />
+			<link rel="ms-icon" sizes="144x144" href="/ms-icon-144x144.png" />
+			<link rel="ms-icon" sizes="150x150" href="/ms-icon-150x150.png" />
+			<link rel="ms-icon" sizes="310x310" href="/ms-icon-310x310.png" />
+
+			<meta name="msapplication-TileColor" content="#ffffff" />
+			<meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+			<meta name="theme-color" content="#ffffff"></meta>
 		</Head>
 	);
 }
