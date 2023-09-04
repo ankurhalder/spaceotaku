3de735f92a403bddc3bdb0919bed167273160069
Ankur Halder <ankur.halder12345@gmail.com>
2023-09-04 17:39:23 +0530

    sidenavbar temporarily ready

--- container/sidenavbar/SideNavBar.jsx
+++ container/sidenavbar/SideNavBar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import Link from "next/link";
 const SideNavbar = () => {
 	const [isOpen, setIsOpen] = useState(false);
 
@@ -25,62 +25,62 @@ const SideNavbar = () => {
 					<span className="tooltip">Search</span>
 				</li>
 				<li>
-					<a href="#">
+					<Link href="/">
 						<i className="bx bx-grid-alt"></i>
 						<span className="links_name">Dashboard</span>
-					</a>
+					</Link>
 					<span className="tooltip">Dashboard</span>
 				</li>
 				<li>
-					<a href="#">
+					<Link href="/">
 						<i className="bx bx-user"></i>
 						<span className="links_name">User</span>
-					</a>
+					</Link>
 					<span className="tooltip">User</span>
 				</li>
 				<li>
-					<a href="#">
+					<Link href="/">
 						<i className="bx bx-chat"></i>
 						<span className="links_name">Messages</span>
-					</a>
+					</Link>
 					<span className="tooltip">Messages</span>
 				</li>
 				<li>
-					<a href="#">
+					<Link href="/">
 						<i className="bx bx-pie-chart-alt-2"></i>
 						<span className="links_name">Analytics</span>
-					</a>
+					</Link>
 					<span className="tooltip">Analytics</span>
 				</li>
 				<li>
-					<a href="#">
+					<Link href="/">
 						<i className="bx bx-folder"></i>
 						<span className="links_name">File Manager</span>
-					</a>
+					</Link>
 					<span className="tooltip">Files</span>
 				</li>
 				<li>
-					<a href="#">
+					<Link href="/">
 						<i className="bx bx-cart-alt"></i>
 						<span className="links_name">Order</span>
-					</a>
+					</Link>
 					<span className="tooltip">Order</span>
 				</li>
 				<li>
-					<a href="#">
+					<Link href="/">
 						<i className="bx bx-heart"></i>
 						<span className="links_name">Saved</span>
-					</a>
+					</Link>
 					<span className="tooltip">Saved</span>
 				</li>
 				<li>
-					<a href="#">
+					<Link href="/">
 						<i className="bx bx-cog"></i>
 						<span className="links_name">Setting</span>
-					</a>
+					</Link>
 					<span className="tooltip">Setting</span>
 				</li>
-				<li className="profile">
+				<li className={`profile ${isOpen ? "open" : ""}`}>
 					<div className="profile-details">
 						{/* <img src="/public/sidenavbar/profile.jpeg" alt="profileImg" /> */}
 						<div className="name_job">
