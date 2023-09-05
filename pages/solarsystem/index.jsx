4514fcee5257facaf1385fcca2a52322ba628e96
Ankur Halder <ankur.halder12345@gmail.com>
2023-09-06 00:14:57 +0530

    some modifications

--- pages/solarsystem/index.jsx
+++ pages/solarsystem/index.jsx
@@ -1,12 +1,16 @@
 import Layout from "@/layout/Layout";
+import { Fragment } from "react";
 export default function SolarSystem() {
 	return (
-		<Layout>
-			<iframe
-				className="solar-system-container"
-				src="https://spaceotakusolarsystem.netlify.app/"
-				title="Space Otaku Solar System"
-			></iframe>
-		</Layout>
+		<Fragment>
+			<h1 className="solar-sysytem-heading">Solar System By Space Otaku</h1>
+			<Layout>
+				<iframe
+					className="solar-system-container"
+					src="https://spaceotakusolarsystem.netlify.app/"
+					title="Space Otaku Solar System"
+				></iframe>
+			</Layout>
+		</Fragment>
 	);
 }
