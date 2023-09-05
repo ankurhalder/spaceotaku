4c86d60f81f83f19c74b68f918f8031c8854ff11
Ankur Halder <ankur.halder12345@gmail.com>
2023-09-05 21:51:56 +0530

    sidenavabar working

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,5 +1,4 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import { CommonMeta, IconMeta } from "@/components";
 export default function Document() {
 	return (
 		<Html lang="en">
