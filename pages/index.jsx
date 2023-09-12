15010fefd192052c88c2e4cde2a475d515ba73f5
Ankur Halder <ankur.halder12345@gmail.com>
2023-09-13 02:13:33 +0530

    working on responsive

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import Head from "next/head";
 import { Fragment, useState, useEffect } from "react";
 import { Articles, Blogs, Reports } from "@/components";
@@ -23,83 +24,30 @@ function Index() {
 				/>
 				<link rel="canonical" href="https://www.spaceotaku.online/" />
 			</Head>
-			{/* <div className="landing-page-sample-contents">
+			<div className="landing-page-sample-contents">
 				<h1>Space Otaku: Navigating the Cosmos for Space Enthusiasts</h1>
-				<h3>Created by Ankur Halder</h3>
-				<h4>
+				<span>Created by Ankur Halder</span>
+				<h5>
 					Embark on a Celestial Journey with Space Otaku: Your Source for Space
 					Enthusiasts
-				</h4>
-				<h5>Explore the Solar System Insights</h5>
-				<h6> Keep tabs on the orbits by satellites</h6>
+				</h5>
+
 				<p>
-					Are you ready to embark on an awe-inspiring cosmic adventure? Space
-					Otaku is your trusted portal for exploring the wonders of the
-					universe, offering a rich trove of information and the latest cosmic
-					updates, all within easy reach.
+					Space Otaku is a dynamic online platform that not only offers a rich
+					array of space-related content but also engages users with an
+					interactive space quiz feature. This quiz comes with three difficulty
+					modes, allowing participants to tailor their cosmic knowledge
+					challenges. What sets Space Otaku apart is its leaderboard, promoting
+					friendly competition and community engagement as users can compare
+					their quiz scores with others. Additionally, the platform provides a
+					dedicated "Quiz Facts" page, enhancing the educational aspect of
+					quizzes by offering insightful explanations and information related to
+					each question. In summary, Space Otaku combines informative articles,
+					interactive quizzes, and opportunities for learning and competition,
+					making it a comprehensive and engaging hub for space enthusiasts of
+					all levels.
 				</p>
-				<p>
-					At Space Otaku, we&aposre driven by an unquenchable passion for
-					everything space-related. From the dazzling beauty of distant
-					celestial bodies to the incredible feats of astronauts venturing
-					beyond our planet&aposs boundaries, we&aposre here to share our wonder
-					with you and make the cosmos accessible to all.
-				</p>
-				<p>
-					What awaits you at Space Otaku? Here&aposs a sneak peek of what we
-					have in store:
-				</p>
-				<ul>
-					<li>
-						<strong>Space News:</strong> Stay up-to-date with the latest cosmic
-						discoveries, mission updates, and celestial events happening in our
-						ever-expanding universe.
-					</li>
-					<li>
-						<strong>Astronaut Profiles:</strong> Get to know the remarkable
-						individuals who journey into the cosmos. Explore their biographies,
-						missions, and contributions to space exploration.
-					</li>
-					<li>
-						<strong>Solar System Insights:</strong> Dive deep into the celestial
-						neighborhood that is our solar system. Explore planets, moons,
-						asteroids, comets, and more, and learn about their fascinating
-						properties and histories.
-					</li>
-					<li>
-						<strong>Satellite Tracking:</strong> Keep tabs on the orbits and
-						functions of satellites circling our planet, providing vital data
-						and facilitating global communication.
-					</li>
-					<li>
-						<strong>Cosmic Discoveries:</strong> Unearth the latest findings and
-						breakthroughs in the field of astronomy and space science, shedding
-						light on the mysteries of the cosmos.
-					</li>
-					<li>
-						<strong>Stargazing Guides:</strong> Enhance your stargazing
-						experience with our helpful guides, showcasing celestial events,
-						constellations, and tips for amateur astronomers.
-					</li>
-					<li>
-						<strong>Interactive Tools:</strong> Engage with interactive tools
-						and simulations that allow you to explore space phenomena and
-						celestial objects from the comfort of your screen.
-					</li>
-				</ul>
-				<p>
-					Join us on an extraordinary journey as we unravel the boundless
-					mysteries of the universe, from the dazzling beauty of distant
-					galaxies to the challenges and triumphs of human spaceflight. Whether
-					you&aposre a seasoned stargazer or a curious newcomer, Space Otaku
-					welcomes all cosmic enthusiasts to share in the wonder and excitement
-					of the final frontier.
-				</p>
-				<p>
-					Are you ready to embark on this cosmic voyage? Let&aposs explore the
-					cosmos together!
-				</p>
-			</div> */}
+			</div>
 
 			<div className="landing-container">
 				<nav className="tab-nav">
