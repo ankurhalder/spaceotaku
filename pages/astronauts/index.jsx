936d689acf24284ab7dc11a7e55147247bf8d4ab
Ankur Halder <ankur.halder12345@gmail.com>
2023-09-19 22:19:54 +0530

    astrounauts in space in space first version

--- pages/astronauts/index.jsx
+++ pages/astronauts/index.jsx
@@ -1,7 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Astronauts() {
-	return <div></div>;
-}
+import astronauts from "@/data/astronauts"; // Import the astronauts data
 
-export default Astronauts;
+const Slider = () => {
+	const [currentIndex, setCurrentIndex] = useState(0);
+
+	const nextSlide = () => {
+		setCurrentIndex((prevIndex) => (prevIndex + 1) % astronauts.people.length);
+	};
+
+	const prevSlide = () => {
+		setCurrentIndex(
+			(prevIndex) =>
+				(prevIndex - 1 + astronauts.people.length) % astronauts.people.length
+		);
+	};
+
+	const astronaut = astronauts.people[currentIndex]; // Get the current astronaut data
+
+	return (
+		<div className="slider-container">
+			<div className="slider">
+				{astronauts.people.map((person, index) => (
+					<div
+						className={`slide ${index === currentIndex ? "active" : ""}`}
+						key={index}
+						style={{
+							transform: `rotateY(${
+								(index - currentIndex) * 45
+							}deg) translateZ(300px)`,
+						}}
+					>
+						<img src={person.image_url} alt={person.name} />
+						<div className="slide-content">
+							<h2>{person.name}</h2>
+							<p>Nationality: {person.nationality}</p>
+							{/* Add more astronaut information here */}
+						</div>
+					</div>
+				))}
+			</div>
+			<button className="prev-button" onClick={prevSlide}>
+				Previous
+			</button>
+			<button className="next-button" onClick={nextSlide}>
+				Next
+			</button>
+		</div>
+	);
+};
+
+export default Slider;
