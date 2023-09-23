fb27d159d5a4ba1c1d9ebbc4d93fc7340c4177aa
Ankur Halder <ankur.halder12345@gmail.com>
2023-09-23 13:57:36 +0530

    icons updated

--- components/IconMeta.jsx
+++ components/IconMeta.jsx
@@ -11,122 +11,121 @@ export default function IconMeta() {
 			<link
 				rel="icon"
 				type="image/png"
-				href="/icon/favicon-32x32.png"
+				href="/icons/favicon-32x32.png"
 				sizes="32x32"
 			></link>
 			<link
 				rel="icon"
 				type="image/png"
-				href="/icon/favicon-16x16.png"
+				href="/icons/favicon-16x16.png"
 				sizes="16x16"
 			></link>
 			<link
 				rel="icon"
 				type="image/png"
 				sizes="96x96"
-				href="/icon/favicon-96x96.png"
+				href="/icons/favicon-96x96.png"
 			/>
 			<link
 				rel="apple-touch-icon"
 				sizes="57x57"
-				href="/icon/apple-icon-57x57.png"
+				href="/icons/apple-icon-57x57.png"
 			/>
 			<link
 				rel="apple-touch-icon"
 				sizes="60x60"
-				href="/icon/apple-icon-60x60.png"
+				href="/icons/apple-icon-60x60.png"
 			/>
 			<link
 				rel="apple-touch-icon"
 				sizes="72x72"
-				href="/icon/apple-icon-72x72.png"
+				href="/icons/apple-icon-72x72.png"
 			/>
 			<link
 				rel="apple-touch-icon"
 				sizes="76x76"
-				href="/icon/apple-icon-76x76.png"
+				href="/icons/apple-icon-76x76.png"
 			/>
 			<link
 				rel="apple-touch-icon"
 				sizes="114x114"
-				href="/icon/apple-icon-114x114.png"
+				href="/icons/apple-icon-114x114.png"
 			/>
 			<link
 				rel="apple-touch-icon"
 				sizes="120x120"
-				href="/icon/apple-icon-120x120.png"
+				href="/icons/apple-icon-120x120.png"
 			/>
 			<link
 				rel="apple-touch-icon"
 				sizes="144x144"
-				href="/icon/apple-icon-144x144.png"
+				href="/icons/apple-icon-144x144.png"
 			/>
 			<link
 				rel="apple-touch-icon"
 				sizes="152x152"
-				href="/icon/apple-icon-152x152.png"
+				href="/icons/apple-icon-152x152.png"
 			/>
 			<link
 				rel="apple-touch-icon"
 				sizes="180x180"
-				href="/icon/apple-icon-180x180.png"
+				href="/icons/apple-icon-180x180.png"
 			/>
 			<link
 				rel="android-icon"
 				sizes="36x36"
-				href="/icon/android-icon-36x36.png"
+				href="/icons/android-icon-36x36.png"
 			/>
 			<link
 				rel="android-icon"
 				sizes="48x48"
-				href="/icon/android-icon-48x48.png"
+				href="/icons/android-icon-48x48.png"
 			/>
 			<link
 				rel="android-icon"
 				sizes="72x72"
-				href="/icon/android-icon-72x72.png"
+				href="/icons/android-icon-72x72.png"
 			/>
 			<link
 				rel="android-icon"
 				sizes="96x96"
-				href="/icon/android-icon-96x96.png"
+				href="/icons/android-icon-96x96.png"
 			/>
 			<link
 				rel="android-icon"
 				sizes="144x144"
-				href="/icon/android-icon-144x144.png"
+				href="/icons/android-icon-144x144.png"
 			/>
 
-			<link rel="icon" sizes="192x192" href="/icon/android-icon-192x192.png" />
+			<link rel="icon" sizes="192x192" href="/icons/android-icon-192x192.png" />
 			<link
 				rel="icon"
 				type="image/png"
 				sizes="32x32"
-				href="/icon/favicon-32x32.png"
+				href="/icons/favicon-32x32.png"
 			/>
 			<link
 				rel="icon"
 				type="image/png"
 				sizes="96x96"
-				href="/icon/favicon-96x96.png"
+				href="/icons/favicon-96x96.png"
 			/>
 			<link
 				rel="icon"
 				type="image/png"
 				sizes="16x16"
-				href="/icon/favicon-16x16.png"
+				href="/icons/favicon-16x16.png"
 			/>
-			<link rel="ms-icon" sizes="70x70" href="/icon/ms-icon-70x70.png" />
-			<link rel="ms-icon" sizes="144x144" href="/icon/ms-icon-144x144.png" />
-			<link rel="ms-icon" sizes="150x150" href="/icon/ms-icon-150x150.png" />
-			<link rel="ms-icon" sizes="310x310" href="/icon/ms-icon-310x310.png" />
+			<link rel="ms-icon" sizes="70x70" href="/icons/ms-icon-70x70.png" />
+			<link rel="ms-icon" sizes="144x144" href="/icons/ms-icon-144x144.png" />
+			<link rel="ms-icon" sizes="150x150" href="/icons/ms-icon-150x150.png" />
+			<link rel="ms-icon" sizes="310x310" href="/icons/ms-icon-310x310.png" />
 
 			<meta name="msapplication-TileColor" content="#ffffff" />
 			<meta
 				name="msapplication-TileImage"
-				content="/icon/ms-icon-144x144.png"
+				content="/icons/ms-icon-144x144.png"
 			/>
-			<meta name="theme-color" content="#000000"></meta>
 		</Head>
 	);
 }
