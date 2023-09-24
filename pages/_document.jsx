bae381510a35cfdc220755ced3db52d673d8ad33
Ankur Halder <ankur.halder12345@gmail.com>
2023-09-24 16:17:46 +0530

    vercel deployment added

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,5 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
-
+import { Analytics } from "@vercel/analytics/react";
 export default function Document() {
 	return (
 		<Html lang="en">
@@ -14,6 +14,7 @@ export default function Document() {
 			<body>
 				<Main />
 				<NextScript />
+				<Analytics />
 			</body>
 		</Html>
 	);
