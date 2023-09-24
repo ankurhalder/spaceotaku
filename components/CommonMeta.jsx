9cbeb738473145c3ab17976489feefedd96207e6
Ankur Halder <ankur.halder12345@gmail.com>
2023-09-24 23:09:09 +0530

    user scalable fix

--- components/CommonMeta.jsx
+++ components/CommonMeta.jsx
@@ -6,7 +6,7 @@ export default function CommonMeta() {
 			<meta charSet="UTF-8" />
 			<meta
 				name="viewport"
-				content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+				content="width=device-width, initial-scale=1.0, maximum-scale=5.0, user-scalable=yes"
 			/>
 			<meta name="author" content="Ankur Halder" />
 			<meta name="robots" content="index, follow" />
