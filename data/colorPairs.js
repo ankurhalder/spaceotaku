9c040449870d6879bf32c06fd7cd5442ce0ccccc
Ankur Halder <ankur.halder12345@gmail.com>
2023-09-29 15:57:49 +0530

    space facts color's are updated

--- data/colorPairs.js
+++ data/colorPairs.js
@@ -1,343 +1,103 @@
 const colorPairs = [
 	{
-		backgroundGradient: "linear-gradient(to right, #FF5733, #FFBD33)",
-		buttonGradient: "linear-gradient(to right, #FF5733, #FFBD33)",
+		backgroundGradient: "linear-gradient(to right, #191970, #800080)", // Midnight Blue to Royal Purple
+		buttonGradient: "linear-gradient(to right, #191970, #800080)", // Midnight Blue to Royal Purple
 		index: 0,
 	},
 	{
-		backgroundGradient: "linear-gradient(to right, #33FFC3, #3392FF)",
-		buttonGradient: "linear-gradient(to right, #33FFC3, #3392FF)",
+		backgroundGradient: "linear-gradient(to right, #00FFFF, #007FFF)", // Electric Blue to Aqua
+		buttonGradient: "linear-gradient(to right, #00FFFF, #007FFF)", // Electric Blue to Aqua
 		index: 1,
 	},
 	{
-		backgroundGradient: "linear-gradient(to right, #FF33F9, #3333FF)",
-		buttonGradient: "linear-gradient(to right, #FF33F9, #3333FF)",
+		backgroundGradient: "linear-gradient(to right, #DC143C, #FF007F)", // Crimson Red to Rose Pink
+		buttonGradient: "linear-gradient(to right, #DC143C, #FF007F)", // Crimson Red to Rose Pink
 		index: 2,
 	},
 	{
-		backgroundGradient: "linear-gradient(to right, #A770EF, #CF8BF3)",
-		buttonGradient: "linear-gradient(to right, #A770EF, #CF8BF3)",
+		backgroundGradient: "linear-gradient(to right, #008000, #00FFC0)", // Emerald Green to Mint Green
+		buttonGradient: "linear-gradient(to right, #008000, #00FFC0)", // Emerald Green to Mint Green
 		index: 3,
 	},
 	{
-		backgroundGradient: "linear-gradient(to right, #FF6B6B, #FFD166)",
-		buttonGradient: "linear-gradient(to right, #FF6B6B, #FFD166)",
+		backgroundGradient: "linear-gradient(to right, #FFD700, #FFA500)", // Golden Yellow to Tangerine Orange
+		buttonGradient: "linear-gradient(to right, #FFD700, #FFA500)", // Golden Yellow to Tangerine Orange
 		index: 4,
 	},
 	{
-		backgroundGradient: "linear-gradient(to right, #4BC0C8, #C779D0, #FEAC5E)",
-		buttonGradient: "linear-gradient(to right, #4BC0C8, #C779D0, #FEAC5E)",
+		backgroundGradient: "linear-gradient(to right, #E6E6FA, #87CEEB)", // Lavender to Sky Blue
+		buttonGradient: "linear-gradient(to right, #E6E6FA, #87CEEB)", // Lavender to Sky Blue
 		index: 5,
 	},
 	{
-		backgroundGradient: "linear-gradient(to right, #667eea, #764ba2)",
-		buttonGradient: "linear-gradient(to right, #667eea, #764ba2)",
+		backgroundGradient: "linear-gradient(to right, #E0115F, #FF4500)", // Ruby Red to Deep Orange
+		buttonGradient: "linear-gradient(to right, #E0115F, #FF4500)", // Ruby Red to Deep Orange
 		index: 6,
 	},
 	{
-		backgroundGradient: "linear-gradient(to right, #20E2D7, #F9FEA5)",
-		buttonGradient: "linear-gradient(to right, #20E2D7, #F9FEA5)",
+		backgroundGradient: "linear-gradient(to right, #008080, #2E0854)", // Teal Blue to Deep Indigo
+		buttonGradient: "linear-gradient(to right, #008080, #2E0854)", // Teal Blue to Deep Indigo
 		index: 7,
 	},
 	{
-		backgroundGradient: "linear-gradient(to right, #B3FFAB, #12FFF7)",
-		buttonGradient: "linear-gradient(to right, #B3FFAB, #12FFF7)",
+		backgroundGradient: "linear-gradient(to right, #FFD700, #FFDAB9)", // Sunflower Yellow to Peach
+		buttonGradient: "linear-gradient(to right, #FFD700, #FFDAB9)", // Sunflower Yellow to Peach
 		index: 8,
 	},
 	{
-		backgroundGradient: "linear-gradient(to right, #FFD700, #F0E68C)",
-		buttonGradient: "linear-gradient(to right, #FFD700, #F0E68C)",
+		backgroundGradient: "linear-gradient(to right, #9932CC, #C8A2C8)", // Orchid Purple to Lilac
+		buttonGradient: "linear-gradient(to right, #9932CC, #C8A2C8)", // Orchid Purple to Lilac
 		index: 9,
 	},
 	{
-		backgroundGradient: "linear-gradient(to right, #FA709A, #FDDE6B)",
-		buttonGradient: "linear-gradient(to right, #FA709A, #FDDE6B)",
+		backgroundGradient: "linear-gradient(to right, #00008B, #00FFFF)", // Deep Blue to Electric Teal
+		buttonGradient: "linear-gradient(to right, #00008B, #00FFFF)", // Deep Blue to Electric Teal
 		index: 10,
 	},
 	{
-		backgroundGradient: "linear-gradient(to right, #25AAE1, #4481EB)",
-		buttonGradient: "linear-gradient(to right, #25AAE1, #4481EB)",
+		backgroundGradient: "linear-gradient(to right, #8B0000, #9966CC)", // Dark Crimson to Amethyst
+		buttonGradient: "linear-gradient(to right, #8B0000, #9966CC)", // Dark Crimson to Amethyst
 		index: 11,
 	},
 	{
-		backgroundGradient: "linear-gradient(to right, #00dbde, #fc00ff)",
-		buttonGradient: "linear-gradient(to right, #00dbde, #fc00ff)",
+		backgroundGradient: "linear-gradient(to right, #191970, #000080)", // Midnight Purple to Galaxy Blue
+		buttonGradient: "linear-gradient(to right, #191970, #000080)", // Midnight Purple to Galaxy Blue
 		index: 12,
 	},
 	{
-		backgroundGradient: "linear-gradient(to right, #00d2ff, #928DAB)",
-		buttonGradient: "linear-gradient(to right, #00d2ff, #928DAB)",
+		backgroundGradient: "linear-gradient(to right, #228B22, #7CFC00)", // Enchanted Forest to Moss Green
+		buttonGradient: "linear-gradient(to right, #228B22, #7CFC00)", // Enchanted Forest to Moss Green
 		index: 13,
 	},
 	{
-		backgroundGradient: "linear-gradient(to right, #e55d87, #5FC3E4)",
-		buttonGradient: "linear-gradient(to right, #e55d87, #5FC3E4)",
+		backgroundGradient: "linear-gradient(to right, #C71585, #FF6EB4)", // Crimson Rose to Blush Pink
+		buttonGradient: "linear-gradient(to right, #C71585, #FF6EB4)", // Crimson Rose to Blush Pink
 		index: 14,
 	},
 	{
-		backgroundGradient: "linear-gradient(to right, #000C40, #607D8B)",
-		buttonGradient: "linear-gradient(to right, #000C40, #607D8B)",
+		backgroundGradient: "linear-gradient(to right, #FFA500, #DAA520)", // Dark Amber to Goldenrod
+		buttonGradient: "linear-gradient(to right, #FFA500, #DAA520)", // Dark Amber to Goldenrod
 		index: 15,
 	},
 	{
-		backgroundGradient: "linear-gradient(to right, #614385, #516395)",
-		buttonGradient: "linear-gradient(to right, #614385, #516395)",
+		backgroundGradient: "linear-gradient(to right, #082E63, #00FFFF)", // Sapphire Blue to Aqua Marine
+		buttonGradient: "linear-gradient(to right, #082E63, #00FFFF)", // Sapphire Blue to Aqua Marine
 		index: 16,
 	},
 	{
-		backgroundGradient: "linear-gradient(to right, #FC466B, #3F5EFB)",
-		buttonGradient: "linear-gradient(to right, #FC466B, #3F5EFB)",
+		backgroundGradient: "linear-gradient(to right, #4B0082, #C8A2C8)", // Indigo Violet to Lilac
+		buttonGradient: "linear-gradient(to right, #4B0082, #C8A2C8)", // Indigo Violet to Lilac
 		index: 17,
 	},
 	{
-		backgroundGradient: "linear-gradient(to right, #DD3E54, #6EE7B7)",
-		buttonGradient: "linear-gradient(to right, #DD3E54, #6EE7B7)",
+		backgroundGradient: "linear-gradient(to right, #FF6B6B, #FA8072)", // Coral Pink to Salmon
+		buttonGradient: "linear-gradient(to right, #FF6B6B, #FA8072)", // Coral Pink to Salmon
 		index: 18,
 	},
 	{
-		backgroundGradient: "linear-gradient(to right, #D31027, #EA384D)",
-		buttonGradient: "linear-gradient(to right, #D31027, #EA384D)",
+		backgroundGradient: "linear-gradient(to right, #2E2EFE, #000033)", // Celestial Blue to Deep Space
+		buttonGradient: "linear-gradient(to right, #2E2EFE, #000033)", // Celestial Blue to Deep Space
 		index: 19,
-	},
-	{
-		backgroundGradient: "linear-gradient(to right, #FA71CD, #C471F5)",
-		buttonGradient: "linear-gradient(to right, #FA71CD, #C471F5)",
-		index: 20,
-	},
-	{
-		backgroundGradient: "linear-gradient(to right, #F79465, #F16529)",
-		buttonGradient: "linear-gradient(to right, #F79465, #F16529)",
-		index: 21,
-	},
-	{
-		backgroundGradient: "linear-gradient(to right, #2C3E50, #FD746C)",
-		buttonGradient: "linear-gradient(to right, #2C3E50, #FD746C)",
-		index: 22,
-	},
-	{
-		backgroundGradient: "linear-gradient(to right, #D1913C, #FFD194)",
-		buttonGradient: "linear-gradient(to right, #D1913C, #FFD194)",
-		index: 23,
-	},
-	{
-		backgroundGradient: "linear-gradient(to right, #355C7D, #6C5B7B, #C06C84)",
-		buttonGradient: "linear-gradient(to right, #355C7D, #6C5B7B, #C06C84)",
-		index: 24,
-	},
-	{
-		backgroundGradient: "linear-gradient(to right, #DA22FF, #9733EE)",
-		buttonGradient: "linear-gradient(to right, #DA22FF, #9733EE)",
-		index: 25,
-	},
-	{
-		backgroundGradient: "linear-gradient(to right, #FC354C, #0ABFBC)",
-		buttonGradient: "linear-gradient(to right, #FC354C, #0ABFBC)",
-		index: 26,
-	},
-	{
-		backgroundGradient: "linear-gradient(to right, #85FFBD, #FFFB7D)",
-		buttonGradient: "linear-gradient(to right, #85FFBD, #FFFB7D)",
-		index: 27,
-	},
-	{
-		backgroundGradient: "linear-gradient(to right, #00B4DB, #0083B0, #00B4DB)",
-		buttonGradient: "linear-gradient(to right, #00B4DB, #0083B0, #00B4DB)",
-		index: 28,
-	},
-	{
-		backgroundGradient: "linear-gradient(to right, #22c1c3, #fdbb2d)",
-		buttonGradient: "linear-gradient(to right, #22c1c3, #fdbb2d)",
-		index: 29,
-	},
-	{
-		backgroundGradient: "linear-gradient(to right, #642B73, #C6426E)",
-		buttonGradient: "linear-gradient(to right, #642B73, #C6426E)",
-		index: 30,
-	},
-	{
-		backgroundGradient: "linear-gradient(to right, #ff9966, #ff5e62)",
-		buttonGradient: "linear-gradient(to right, #ff9966, #ff5e62)",
-		index: 31,
-	},
-	{
-		backgroundGradient: "linear-gradient(to right, #3a7bd5, #3a6073)",
-		buttonGradient: "linear-gradient(to right, #3a7bd5, #3a6073)",
-		index: 32,
-	},
-	{
-		backgroundGradient: "linear-gradient(to right, #0D324D, #7F5A83)",
-		buttonGradient: "linear-gradient(to right, #0D324D, #7F5A83)",
-		index: 33,
-	},
-	{
-		backgroundGradient: "linear-gradient(to right, #C33764, #1D2671)",
-		buttonGradient: "linear-gradient(to right, #C33764, #1D2671)",
-		index: 34,
-	},
-	{
-		backgroundGradient: "linear-gradient(to right, #DAE2F8, #D6A4A4)",
-		buttonGradient: "linear-gradient(to right, #DAE2F8, #D6A4A4)",
-		index: 35,
-	},
-	{
-		backgroundGradient: "linear-gradient(to right, #FC5C7D, #6A82FB)",
-		buttonGradient: "linear-gradient(to right, #FC5C7D, #6A82FB)",
-		index: 36,
-	},
-	{
-		backgroundGradient: "linear-gradient(to right, #E44D26, #F16529)",
-		buttonGradient: "linear-gradient(to right, #E44D26, #F16529)",
-		index: 37,
-	},
-	{
-		backgroundGradient: "linear-gradient(to right, #ff9966, #ff5e62)",
-		buttonGradient: "linear-gradient(to right, #ff9966, #ff5e62)",
-		index: 38,
-	},
-	{
-		backgroundGradient: "linear-gradient(to right, #A770EF, #CF8BF3)",
-		buttonGradient: "linear-gradient(to right, #A770EF, #CF8BF3)",
-		index: 39,
-	},
-	{
-		backgroundGradient: "linear-gradient(to right, #F2994A, #F2C94C)",
-		buttonGradient: "linear-gradient(to right, #F2994A, #F2C94C)",
-		index: 40,
-	},
-	{
-		backgroundGradient: "linear-gradient(to right, #8e9eab, #eef2f3)",
-		buttonGradient: "linear-gradient(to right, #8e9eab, #eef2f3)",
-		index: 41,
-	},
-	{
-		backgroundGradient: "linear-gradient(to right, #A6A9E2, #1EAE98)",
-		buttonGradient: "linear-gradient(to right, #A6A9E2, #1EAE98)",
-		index: 42,
-	},
-	{
-		backgroundGradient: "linear-gradient(to right, #0B486B, #F56217)",
-		buttonGradient: "linear-gradient(to right, #0B486B, #F56217)",
-		index: 43,
-	},
-	{
-		backgroundGradient: "linear-gradient(to right, #DA4453, #89216B)",
-		buttonGradient: "linear-gradient(to right, #DA4453, #89216B)",
-		index: 44,
-	},
-	{
-		backgroundGradient: "linear-gradient(to right, #6a11cb, #2575fc)",
-		buttonGradient: "linear-gradient(to right, #6a11cb, #2575fc)",
-		index: 45,
-	},
-	{
-		backgroundGradient: "linear-gradient(to right, #DAE2F8, #D6A4A4)",
-		buttonGradient: "linear-gradient(to right, #DAE2F8, #D6A4A4)",
-		index: 46,
-	},
-	{
-		backgroundGradient: "linear-gradient(to right, #FC5C7D, #6A82FB)",
-		buttonGradient: "linear-gradient(to right, #FC5C7D, #6A82FB)",
-		index: 47,
-	},
-	{
-		backgroundGradient: "linear-gradient(to right, #E44D26, #F16529)",
-		buttonGradient: "linear-gradient(to right, #E44D26, #F16529)",
-		index: 48,
-	},
-	{
-		backgroundGradient: "linear-gradient(to right, #ff9966, #ff5e62)",
-		buttonGradient: "linear-gradient(to right, #ff9966, #ff5e62)",
-		index: 49,
-	},
-	{
-		backgroundGradient: "linear-gradient(to right, #A770EF, #CF8BF3)",
-		buttonGradient: "linear-gradient(to right, #A770EF, #CF8BF3)",
-		index: 50,
-	},
-	{
-		backgroundGradient: "linear-gradient(to right, #F2994A, #F2C94C)",
-		buttonGradient: "linear-gradient(to right, #F2994A, #F2C94C)",
-		index: 51,
-	},
-	{
-		backgroundGradient: "linear-gradient(to right, #8e9eab, #eef2f3)",
-		buttonGradient: "linear-gradient(to right, #8e9eab, #eef2f3)",
-		index: 52,
-	},
-	{
-		backgroundGradient: "linear-gradient(to right, #A6A9E2, #1EAE98)",
-		buttonGradient: "linear-gradient(to right, #A6A9E2, #1EAE98)",
-		index: 53,
-	},
-	{
-		backgroundGradient: "linear-gradient(to right, #0B486B, #F56217)",
-		buttonGradient: "linear-gradient(to right, #0B486B, #F56217)",
-		index: 54,
-	},
-	{
-		backgroundGradient: "linear-gradient(to right, #DA4453, #89216B)",
-		buttonGradient: "linear-gradient(to right, #DA4453, #89216B)",
-		index: 55,
-	},
-	{
-		backgroundGradient: "linear-gradient(to right, #6a11cb, #2575fc)",
-		buttonGradient: "linear-gradient(to right, #6a11cb, #2575fc)",
-		index: 56,
-	},
-	{
-		backgroundGradient: "linear-gradient(to right, #DAE2F8, #D6A4A4)",
-		buttonGradient: "linear-gradient(to right, #DAE2F8, #D6A4A4)",
-		index: 57,
-	},
-	{
-		backgroundGradient: "linear-gradient(to right, #FC5C7D, #6A82FB)",
-		buttonGradient: "linear-gradient(to right, #FC5C7D, #6A82FB)",
-		index: 58,
-	},
-	{
-		backgroundGradient: "linear-gradient(to right, #E44D26, #F16529)",
-		buttonGradient: "linear-gradient(to right, #E44D26, #F16529)",
-		index: 59,
-	},
-	{
-		backgroundGradient: "linear-gradient(to right, #ff9966, #ff5e62)",
-		buttonGradient: "linear-gradient(to right, #ff9966, #ff5e62)",
-		index: 60,
-	},
-	{
-		backgroundGradient: "linear-gradient(to right, #A770EF, #CF8BF3)",
-		buttonGradient: "linear-gradient(to right, #A770EF, #CF8BF3)",
-		index: 61,
-	},
-	{
-		backgroundGradient: "linear-gradient(to right, #F2994A, #F2C94C)",
-		buttonGradient: "linear-gradient(to right, #F2994A, #F2C94C)",
-		index: 62,
-	},
-	{
-		backgroundGradient: "linear-gradient(to right, #8e9eab, #eef2f3)",
-		buttonGradient: "linear-gradient(to right, #8e9eab, #eef2f3)",
-		index: 63,
-	},
-	{
-		backgroundGradient: "linear-gradient(to right, #A6A9E2, #1EAE98)",
-		buttonGradient: "linear-gradient(to right, #A6A9E2, #1EAE98)",
-		index: 64,
-	},
-	{
-		backgroundGradient: "linear-gradient(to right, #0B486B, #F56217)",
-		buttonGradient: "linear-gradient(to right, #0B486B, #F56217)",
-		index: 65,
-	},
-	{
-		backgroundGradient: "linear-gradient(to right, #DA4453, #89216B)",
-		buttonGradient: "linear-gradient(to right, #DA4453, #89216B)",
-		index: 66,
-	},
-	{
-		backgroundGradient: "linear-gradient(to right, #6a11cb, #2575fc)",
-		buttonGradient: "linear-gradient(to right, #6a11cb, #2575fc)",
-		index: 67,
 	},
 ];
 
