d927a7176243b2ffef94bd449c27170c51dc16d8
Ankur Halder <ankur.halder12345@gmail.com>
2023-09-30 18:33:54 +0530

    space quiz initiated

---
+++ data/spaceQuiz.js
@@ -0,0 +1,9 @@
+const spaceQuiz = [
+	{
+		index: 0,
+		difficulty: "easy",
+		question: "What is the name of the first satellite sent into space?",
+		options: ["Sputnik 1", "Explorer 1", "Vanguard 1", "Mariner 1"],
+		correctAnswer: "Sputnik 1",
+	},
+];
