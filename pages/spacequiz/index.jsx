cc71f58089a3f572d72adbf5ad6d3160e8b71fb5
Ankur Halder <ankur.halder12345@gmail.com>
2023-10-01 00:03:31 +0530

    buggissh quiz

--- pages/spacequiz/index.jsx
+++ pages/spacequiz/index.jsx
@@ -1,101 +1,25 @@
-import { useState, useEffect } from "react";
-import shuffle from "lodash/shuffle";
-import uniqueSpaceQuiz from "../../functions/uniqueQuiz";
-
+// SpaceQuizPage.jsx
+import { useState } from "react";
+import { EasyMode, MediumMode, HardMode } from "@/components";
 export default function SpaceQuizPage() {
-	const [questions, setQuestions] = useState([]);
-	const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-	const [score, setScore] = useState(0);
-	const [showResult, setShowResult] = useState(false);
-	const [timeLeft, setTimeLeft] = useState(600); // 10 minutes in seconds
-	const [shuffledOptions, setShuffledOptions] = useState([]);
-
-	useEffect(() => {
-		// Shuffle all unique questions for all modes
-		const shuffledQuestions = shuffle(uniqueSpaceQuiz);
-		setQuestions(shuffledQuestions.slice(0, 10)); // Always select 10 questions
-	}, []);
-
-	useEffect(() => {
-		// Shuffle options only when a new question is displayed
-		if (currentQuestionIndex < questions.length) {
-			const currentQuestion = questions[currentQuestionIndex];
-			setShuffledOptions(shuffle(currentQuestion?.options || []));
-		}
-	}, [currentQuestionIndex, questions]);
-
-	useEffect(() => {
-		if (timeLeft > 0 && !showResult) {
-			const timer = setInterval(() => {
-				setTimeLeft(timeLeft - 1);
-			}, 1000);
-
-			return () => {
-				clearInterval(timer);
-			};
-		} else if (timeLeft === 0) {
-			setShowResult(true);
-		}
-	}, [timeLeft, showResult]);
-
-	const handleAnswerClick = (selectedAnswer) => {
-		const currentQuestion = questions[currentQuestionIndex];
-
-		if (selectedAnswer === currentQuestion.correctAnswer) {
-			setScore(score + 1);
-		}
-
-		if (currentQuestionIndex < 9) {
-			setCurrentQuestionIndex(currentQuestionIndex + 1);
-		} else {
-			setShowResult(true);
-		}
-	};
-
-	const restartQuiz = () => {
-		// Shuffle all questions again to get a new set of 10 questions
-		const shuffledQuestions = shuffle(uniqueSpaceQuiz);
-		setQuestions(shuffledQuestions.slice(0, 10));
-		setCurrentQuestionIndex(0);
-		setScore(0);
-		setShowResult(false);
-		setTimeLeft(600);
-		setShuffledOptions([]);
-	};
-
-	const currentQuestion = questions[currentQuestionIndex];
+	const [selectedDifficulty, setSelectedDifficulty] = useState(null);
 
 	return (
 		<div className="quiz-container">
-			{showResult ? (
-				<div className="result-container">
-					<h2>Your Score: {score} / 10</h2>
-					<button onClick={restartQuiz}>Restart Quiz</button>
+			{!selectedDifficulty ? (
+				<div className="difficulty-selection">
+					<h2>Select Difficulty:</h2>
+					<button onClick={() => setSelectedDifficulty("easy")}>Easy</button>
+					<button onClick={() => setSelectedDifficulty("medium")}>
+						Medium
+					</button>
+					<button onClick={() => setSelectedDifficulty("hard")}>Hard</button>
 				</div>
 			) : (
 				<>
-					{currentQuestion ? (
-						<div className="question-container">
-							<h2>Question {currentQuestionIndex + 1}</h2>
-							<p>{currentQuestion.question}</p>
-							<div className="answer-options">
-								{shuffledOptions.map((option, index) => (
-									<button key={index} onClick={() => handleAnswerClick(option)}>
-										{option}
-									</button>
-								))}
-							</div>
-							<div className="timer">
-								Time Left: {Math.floor(timeLeft / 60)}:
-								{(timeLeft % 60).toString().padStart(2, "0")}
-							</div>
-						</div>
-					) : (
-						<div className="result-container">
-							<h2>Your Score: {score} / 10</h2>
-							<button onClick={restartQuiz}>Restart Quiz</button>
-						</div>
-					)}
+					{selectedDifficulty === "easy" && <EasyMode />}
+					{selectedDifficulty === "medium" && <MediumMode />}
+					{selectedDifficulty === "hard" && <HardMode />}
 				</>
 			)}
 		</div>
