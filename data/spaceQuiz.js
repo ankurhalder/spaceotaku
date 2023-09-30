8b2bf222086147d6df5fe6f5db9805c385683b52
Ankur Halder <ankur.halder12345@gmail.com>
2023-09-30 18:41:11 +0530

    20 questions added

--- data/spaceQuiz.js
+++ data/spaceQuiz.js
@@ -6,4 +6,180 @@ const spaceQuiz = [
 		options: ["Sputnik 1", "Explorer 1", "Vanguard 1", "Mariner 1"],
 		correctAnswer: "Sputnik 1",
 	},
+	{
+		index: 1,
+		difficulty: "medium",
+		question:
+			"Who holds the record for the longest continuous time spent in space by an astronaut?",
+		options: [
+			"Yuri Gagarin",
+			"Valentina Tereshkova",
+			"Peggy Whitson",
+			"John Glenn",
+		],
+		correctAnswer: "Peggy Whitson",
+	},
+	{
+		index: 2,
+		difficulty: "medium",
+		question: "Which planet in our solar system is known as the 'Red Planet'?",
+		options: ["Venus", "Mars", "Jupiter", "Saturn"],
+		correctAnswer: "Mars",
+	},
+	{
+		index: 3,
+		difficulty: "easy",
+		question:
+			"What is the distance from the Earth to the Sun in astronomical units (AU)?",
+		options: [
+			"93 million miles",
+			"93 million kilometers",
+			"1 AU",
+			"149.6 million kilometers",
+		],
+		correctAnswer: "1 AU",
+	},
+	{
+		index: 4,
+		difficulty: "medium",
+		question:
+			"Which space agency is known as the Indian Space Research Organisation?",
+		options: ["NASA", "ESA", "ISRO", "CNSA"],
+		correctAnswer: "ISRO",
+	},
+	{
+		index: 5,
+		difficulty: "medium",
+		question:
+			"How many manned Apollo missions successfully landed on the Moon?",
+		options: ["5", "6", "7", "8"],
+		correctAnswer: "6",
+	},
+	{
+		index: 6,
+		difficulty: "medium",
+		question:
+			"What is the name of the phenomenon where light from a celestial object is stretched and becomes redder as it moves away from an observer?",
+		options: [
+			"Redshift",
+			"Blueshift",
+			"Gravitational lensing",
+			"Solar eclipse",
+		],
+		correctAnswer: "Redshift",
+	},
+	{
+		index: 7,
+		difficulty: "medium",
+		question: "When was the first successful human moon landing by Apollo 11?",
+		options: ["1961", "1969", "1975", "1981"],
+		correctAnswer: "1969",
+	},
+	{
+		index: 8,
+		difficulty: "medium",
+		question:
+			"What is the term for the scientific study of life outside of Earth?",
+		options: [
+			"Astrobiology",
+			"Exobiology",
+			"Extraterrestriology",
+			"Xenobiology",
+		],
+		correctAnswer: "Astrobiology",
+	},
+	{
+		index: 9,
+		difficulty: "easy",
+		question: "What is the name of the largest moon of Saturn?",
+		options: ["Titan", "Io", "Ganymede", "Enceladus"],
+		correctAnswer: "Titan",
+	},
+	{
+		index: 10,
+		difficulty: "medium",
+		question:
+			"Which spacecraft is known for its mission to study Jupiter and its moons?",
+		options: ["Voyager 2", "Hubble Space Telescope", "Galileo", "New Horizons"],
+		correctAnswer: "Galileo",
+	},
+	{
+		index: 11,
+		difficulty: "medium",
+		question:
+			"What is the term for the region of space where gravitational forces are so strong that nothing can escape, not even light?",
+		options: ["Event Horizon", "Asteroid Belt", "Dark Matter", "Black Hole"],
+		correctAnswer: "Black Hole",
+	},
+	{
+		index: 12,
+		difficulty: "easy",
+		question: "Which planet is known for its stunning ring system?",
+		options: ["Earth", "Mars", "Saturn", "Uranus"],
+		correctAnswer: "Saturn",
+	},
+	{
+		index: 13,
+		difficulty: "medium",
+		question:
+			"What is the name of NASA's rover that successfully landed on Mars in February 2021?",
+		options: ["Curiosity", "Opportunity", "Perseverance", "Spirit"],
+		correctAnswer: "Perseverance",
+	},
+	{
+		index: 14,
+		difficulty: "medium",
+		question:
+			"Which space agency is known for launching the Chang'e missions to explore the Moon?",
+		options: ["NASA", "ESA", "ISRO", "CNSA"],
+		correctAnswer: "CNSA",
+	},
+	{
+		index: 15,
+		difficulty: "medium",
+		question:
+			"What is the approximate age of the universe, according to current scientific estimates?",
+		options: [
+			"4.5 billion years",
+			"10 million years",
+			"13.8 billion years",
+			"20 billion years",
+		],
+		correctAnswer: "13.8 billion years",
+	},
+	{
+		index: 16,
+		difficulty: "medium",
+		question:
+			"What is the name of the first American woman to travel in space?",
+		options: [
+			"Valentina Tereshkova",
+			"Sally Ride",
+			"Peggy Whitson",
+			"Mae Jemison",
+		],
+		correctAnswer: "Sally Ride",
+	},
+	{
+		index: 17,
+		difficulty: "medium",
+		question: "Which gas is the most abundant in Earth's atmosphere?",
+		options: ["Oxygen", "Carbon dioxide", "Nitrogen", "Hydrogen"],
+		correctAnswer: "Nitrogen",
+	},
+	{
+		index: 18,
+		difficulty: "medium",
+		question: "What is the name of the nearest galaxy to the Milky Way?",
+		options: ["Andromeda", "Triangulum", "Orion", "Sombrero"],
+		correctAnswer: "Andromeda",
+	},
+	{
+		index: 19,
+		difficulty: "medium",
+		question:
+			"What is the name of the first artificial Earth satellite, launched by the Soviet Union in 1957?",
+		options: ["Apollo 11", "Hubble Space Telescope", "Voyager 1", "Sputnik 1"],
+		correctAnswer: "Sputnik 1",
+	},
 ];
