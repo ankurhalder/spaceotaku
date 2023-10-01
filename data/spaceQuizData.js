ae274cdf09e42d7c0e823d0ea9d42e2dfe803ac1
Ankur Halder <ankur.halder12345@gmail.com>
2023-10-01 16:13:34 +0530

    new data for spacequiz

---
+++ data/spaceQuizData.js
@@ -0,0 +1,29 @@
+const spaceQuizData = [
+	{
+		question: "What is the closest planet to the Sun?",
+		options: ["Earth", "Venus", "Mars", "Mercury"],
+		correctAnswer: "Mercury",
+	},
+	{
+		question: "Who was the first person to walk on the Moon?",
+		options: ["Neil Armstrong", "Buzz Aldrin", "Yuri Gagarin", "John Glenn"],
+		correctAnswer: "Neil Armstrong",
+	},
+	{
+		question: "What is the largest planet in our solar system?",
+		options: ["Mars", "Jupiter", "Saturn", "Uranus"],
+		correctAnswer: "Jupiter",
+	},
+	{
+		question: "Which space agency launched the Hubble Space Telescope?",
+		options: ["NASA", "ESA", "Roscosmos", "ISRO"],
+		correctAnswer: "NASA",
+	},
+	{
+		question: "How many moons does Earth have?",
+		options: ["1", "2", "0", "3"],
+		correctAnswer: "1",
+	},
+];
+
+export default spaceQuizData;
