6c66947bbfad75fc630f10044beb387bc2345305
Ankur Halder <ankur.halder12345@gmail.com>
2023-10-06 15:15:44 +0530

    meta information added for space quiz

--- pages/spacequiz/index.jsx
+++ pages/spacequiz/index.jsx
@@ -1,8 +1,8 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Fragment } from "react";
 import shuffleArray from "@/functions/shuffleArray";
 import shuffledSpaceQuizData from "../../data/spaceQuizData";
-
+import Head from "next/head";
 const initialTimers = {
 	normal: 30,
 	intermediate: 20,
@@ -119,119 +119,143 @@ function SpaceQuiz() {
 	const progress = (currentQuestionIndex / questions.length) * 100;
 
 	return (
-		<div className="quiz-container">
-			<h1 className="Space-quiz-heading">Space Quiz By Space Otaku</h1>
-			{!difficultySelected ? (
-				<div className="difficulty-selection">
-					<h3>Select Difficulty:</h3>
-					<button
-						className="difficulty-button"
-						onClick={() => handleDifficultyChange("normal")}
-					>
-						Normal
-					</button>
-					<button
-						className="difficulty-button"
-						onClick={() => handleDifficultyChange("intermediate")}
-					>
-						Intermediate
-					</button>
-					<button
-						className="difficulty-button"
-						onClick={() => handleDifficultyChange("advanced")}
-					>
-						Advanced
-					</button>
-				</div>
-			) : showScore || quizEnded ? (
-				<div className="result">
-					<span>
-						Your Score: {score} out of {questions.length}
-					</span>
-					<br />
-					{quizEnded && (
-						<button className="restart-button" onClick={handleRestartQuiz}>
-							Restart Quiz
+		<Fragment>
+			<Head>
+				<title>Test Your Space Knowledge | Space Otaku</title>
+				<meta
+					name="description"
+					content="Test your space knowledge. Choose difficulty, answer questions, and check your score!"
+				/>
+				<meta
+					name="keywords"
+					content="space quiz, space trivia, quiz game, Ankur, Halder, Ankur Halder, Space Otaku, Space Otaku by Ankur Halder,"
+				/>
+				<meta
+					property="og:url"
+					content="https://www.spaceotaku.online/spacequiz"
+				/>
+				<meta
+					name="twitter:url"
+					content="https://www.spaceotaku.online/spacequiz"
+				/>
+			</Head>
+			<div className="quiz-container">
+				<h1 className="Space-quiz-heading">Space Quiz By Space Otaku</h1>
+				{!difficultySelected ? (
+					<div className="difficulty-selection">
+						<h3>Select Difficulty:</h3>
+						<button
+							className="difficulty-button"
+							onClick={() => handleDifficultyChange("normal")}
+						>
+							Normal
 						</button>
-					)}
-					<button className="review-button" onClick={() => setReviewMode(true)}>
-						Review Answers
-					</button>
-				</div>
-			) : (
-				<div className="question-container">
-					<h4>Question {currentQuestionIndex + 1} :</h4>
-					<div className="progress-bar">
-						<div className="progress" style={{ width: `${progress}%` }}></div>
+						<button
+							className="difficulty-button"
+							onClick={() => handleDifficultyChange("intermediate")}
+						>
+							Intermediate
+						</button>
+						<button
+							className="difficulty-button"
+							onClick={() => handleDifficultyChange("advanced")}
+						>
+							Advanced
+						</button>
 					</div>
-					{questions[currentQuestionIndex] && (
-						<p>{questions[currentQuestionIndex].question}</p>
-					)}
-					<div className="timer">Time Left: {timer} seconds</div>
-					{hintText && <p className="hint">{hintText}</p>}
-					<div className="answer-options">
-						{questions[currentQuestionIndex]?.options.map((option, index) => (
-							<button
-								key={index}
-								onClick={() => handleAnswerClick(option)}
-								className={`answer-option ${
-									option === selectedAnswer ? "selected" : ""
-								} ${feedback && isQuizDisabled ? "disabled" : ""}
-								${
-									feedback &&
-									option === questions[currentQuestionIndex].correctAnswer
-										? "correct"
-										: ""
-								}`}
-								disabled={isQuizDisabled}
-							>
-								{option}
+				) : showScore || quizEnded ? (
+					<div className="result">
+						<span>
+							Your Score: {score} out of {questions.length}
+						</span>
+						<br />
+						{quizEnded && (
+							<button className="restart-button" onClick={handleRestartQuiz}>
+								Restart Quiz
 							</button>
+						)}
+						<button
+							className="review-button"
+							onClick={() => setReviewMode(true)}
+						>
+							Review Answers
+						</button>
+					</div>
+				) : (
+					<div className="question-container">
+						<h4>Question {currentQuestionIndex + 1} :</h4>
+						<div className="progress-bar">
+							<div className="progress" style={{ width: `${progress}%` }}></div>
+						</div>
+						{questions[currentQuestionIndex] && (
+							<p>{questions[currentQuestionIndex].question}</p>
+						)}
+						<div className="timer">Time Left: {timer} seconds</div>
+						{hintText && <p className="hint">{hintText}</p>}
+						<div className="answer-options">
+							{questions[currentQuestionIndex]?.options.map((option, index) => (
+								<button
+									key={index}
+									onClick={() => handleAnswerClick(option)}
+									className={`answer-option ${
+										option === selectedAnswer ? "selected" : ""
+									} ${feedback && isQuizDisabled ? "disabled" : ""}
+							${
+								feedback &&
+								option === questions[currentQuestionIndex].correctAnswer
+									? "correct"
+									: ""
+							}`}
+									disabled={isQuizDisabled}
+								>
+									{option}
+								</button>
+							))}
+						</div>
+						<button
+							className="hint-button"
+							onClick={() =>
+								handleHintClick(
+									questions[currentQuestionIndex]?.hint || "No hint available."
+								)
+							}
+						>
+							Show Hint
+						</button>
+					</div>
+				)}
+				{reviewMode && (
+					<div className="review-container">
+						<h5>Review Answers:</h5>
+						{questions.map((question, index) => (
+							<div key={index} className="review-item">
+								<p>
+									<strong>Question {index + 1}:</strong> {question.question}
+								</p>
+								<p>
+									<strong>Your Answer:</strong>{" "}
+									{selectedAnswers[index] || "Not attempted"}
+								</p>
+								<p>
+									<strong>Correct Answer:</strong> {question.correctAnswer}
+								</p>
+								<div
+									className={`answer-feedback ${
+										selectedAnswers[index] === question.correctAnswer
+											? "correct"
+											: "incorrect"
+									}`}
+								>
+									{selectedAnswers[index] === question.correctAnswer
+										? "Correct!"
+										: "Incorrect!"}
+								</div>
+							</div>
 						))}
 					</div>
-					<button
-						className="hint-button"
-						onClick={() =>
-							handleHintClick(
-								questions[currentQuestionIndex]?.hint || "No hint available."
-							)
-						}
-					>
-						Show Hint
-					</button>
-				</div>
-			)}
-			{reviewMode && (
-				<div className="review-container">
-					<h5>Review Answers:</h5>
-					{questions.map((question, index) => (
-						<div key={index} className="review-item">
-							<p>
-								<strong>Question {index + 1}:</strong> {question.question}
-							</p>
-							<p>
-								<strong>Your Answer:</strong>{" "}
-								{selectedAnswers[index] || "Not attempted"}
-							</p>
-							<p>
-								<strong>Correct Answer:</strong> {question.correctAnswer}
-							</p>
-							<div
-								className={`answer-feedback ${
-									selectedAnswers[index] === question.correctAnswer
-										? "correct"
-										: "incorrect"
-								}`}
-							>
-								{selectedAnswers[index] === question.correctAnswer
-									? "Correct!"
-									: "Incorrect!"}
-							</div>
-						</div>
-					))}
-				</div>
-			)}
-		</div>
+				)}
+			</div>
+		</Fragment>
 	);
 }
 export default SpaceQuiz;
