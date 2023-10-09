b21135b9b6fc7aedb3a6e19635a9c2bc43370c93
Ankur Halder <ankur.halder12345@gmail.com>
2023-10-09 22:24:27 +0530

    enemies are coming down

--- components/Game/SpaceShooter.jsx
+++ components/Game/SpaceShooter.jsx
@@ -1,12 +1,10 @@
 // components/SpaceShooter.js
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 
 const SpaceShooter = () => {
 	const canvasRef = useRef(null);
 	const contextRef = useRef(null);
-	const enemiesRef = useRef([]);
-	const bulletsRef = useRef([]);
-	const [score, setScore] = useState(0);
+	const enemiesRef = useRef([]); // Array to store enemy objects
 
 	useEffect(() => {
 		const canvas = canvasRef.current;
@@ -23,31 +21,7 @@ const SpaceShooter = () => {
 
 		// Load the spaceship image
 		const spaceshipImg = new Image();
-		spaceshipImg.src = "/spaceship.png";
-
-		// Create a function to handle bullet creation
-		const createBullet = () => {
-			const bullet = {
-				x: playerX + playerWidth / 2 - 2.5,
-				y: playerY,
-				width: 5,
-				height: 20,
-				speed: 5,
-			};
-			bulletsRef.current.push(bullet);
-		};
-
-		// Create a function to handle bullet movement
-		const moveBullets = () => {
-			bulletsRef.current.forEach((bullet, bulletIndex) => {
-				bullet.y -= bullet.speed;
-
-				// Remove bullets that go off the canvas
-				if (bullet.y < 0) {
-					bulletsRef.current.splice(bulletIndex, 1);
-				}
-			});
-		};
+		spaceshipImg.src = "/spaceship.png"; // Assuming it's in the public directory
 
 		// Create a function to add enemies
 		const addEnemy = () => {
@@ -56,12 +30,12 @@ const SpaceShooter = () => {
 				y: 0,
 				width: 40,
 				height: 40,
-				speed: Math.random() * 2 + 1,
+				speed: Math.random() * 2 + 1, // Random enemy speed
 			};
 			enemiesRef.current.push(enemy);
 		};
 
-		// Event listeners for player movement
+		// Handle player movement
 		const handlePlayerMove = (e) => {
 			if (e.key === "ArrowLeft") {
 				playerSpeedX = -playerSpeed;
@@ -79,46 +53,14 @@ const SpaceShooter = () => {
 			playerSpeedY = 0;
 		};
 
-		// Event listener for shooting
-		window.addEventListener("keydown", (e) => {
-			if (e.key === " ") {
-				createBullet();
-			}
-		});
+		// Event listeners for player movement
+		window.addEventListener("keydown", handlePlayerMove);
+		window.addEventListener("keyup", handlePlayerStop);
 
 		// Game loop
 		const gameLoop = () => {
 			// Clear the canvas
 			contextRef.current.clearRect(0, 0, canvas.width, canvas.height);
-
-			// Update and draw enemies
-			enemiesRef.current.forEach((enemy, enemyIndex) => {
-				enemy.y += enemy.speed;
-				contextRef.current.fillStyle = "red";
-				contextRef.current.fillRect(
-					enemy.x,
-					enemy.y,
-					enemy.width,
-					enemy.height
-				);
-
-				// Remove enemies that are out of the canvas
-				if (enemy.y > canvas.height) {
-					enemiesRef.current.splice(enemyIndex, 1);
-				}
-			});
-
-			// Update and draw bullets
-			moveBullets();
-			bulletsRef.current.forEach((bullet) => {
-				contextRef.current.fillStyle = "yellow";
-				contextRef.current.fillRect(
-					bullet.x,
-					bullet.y,
-					bullet.width,
-					bullet.height
-				);
-			});
 
 			// Update player position
 			playerX += playerSpeedX;
@@ -141,27 +83,41 @@ const SpaceShooter = () => {
 				playerHeight
 			);
 
-			// Display the score on the canvas
-			contextRef.current.fillStyle = "white";
-			contextRef.current.font = "24px Arial";
-			contextRef.current.fillText(`Score: ${score}`, 10, 30);
-
-			// Add enemies randomly
-			if (Math.random() < 0.02) {
+			// Add enemies randomly with lower probability (e.g., 1%)
+			if (Math.random() < 0.01) {
 				addEnemy();
 			}
+
+			// Update and draw enemies
+			enemiesRef.current.forEach((enemy, index) => {
+				enemy.y += enemy.speed;
+				contextRef.current.fillStyle = "red";
+				contextRef.current.fillRect(
+					enemy.x,
+					enemy.y,
+					enemy.width,
+					enemy.height
+				);
+
+				// Remove enemies that are out of the canvas
+				if (enemy.y > canvas.height) {
+					enemiesRef.current.splice(index, 1);
+				}
+			});
 
 			// Request the next frame
 			requestAnimationFrame(gameLoop);
 		};
 
-		// Event listeners for player movement
-		window.addEventListener("keydown", handlePlayerMove);
-		window.addEventListener("keyup", handlePlayerStop);
-
 		// Start the game loop
 		gameLoop();
-	}, [score]);
+
+		// Clean up event listeners
+		return () => {
+			window.removeEventListener("keydown", handlePlayerMove);
+			window.removeEventListener("keyup", handlePlayerStop);
+		};
+	}, []);
 
 	return <canvas ref={canvasRef} width={1200} height={800} />;
 };
