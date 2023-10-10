3161935680a0ed5afee94e604d9ba14dcf5bb9a0
Ankur Halder <ankur.halder12345@gmail.com>
2023-10-10 14:03:14 +0530

    bug fix

--- components/Game/SpaceShooter.jsx
+++ components/Game/SpaceShooter.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useRef } from "react";
 
 const SpaceShooter = () => {
@@ -17,8 +18,6 @@ const SpaceShooter = () => {
 	const enemiesRef = useRef([]);
 	const scoreRef = useRef(0);
 	const gameOverRef = useRef(false);
-
-	// Define an array of enemy image paths
 	const enemyImages = [
 		"/aliens/alien-1.png",
 		"/aliens/alien-2.png",
@@ -33,10 +32,8 @@ const SpaceShooter = () => {
 		contextRef.current = context;
 
 		const player = playerRef.current;
-		const playerImageObj = new Image(); // Create an image object
-
-		// Set the source of the player image from the public directory
-		playerImageObj.src = "/spaceship.png"; // Adjust the path as needed
+		const playerImageObj = new Image();
+		playerImageObj.src = "/spaceship.png";
 
 		const enemyImageRefs = enemyImages.map((imagePath) => {
 			const imageRef = new Image();
@@ -47,7 +44,6 @@ const SpaceShooter = () => {
 		const drawPlayer = () => {
 			context.clearRect(0, 0, canvas.width, canvas.height);
 
-			// Draw the player image on the canvas
 			context.drawImage(
 				playerImageObj,
 				player.x,
@@ -73,7 +69,6 @@ const SpaceShooter = () => {
 
 		const drawEnemies = () => {
 			enemiesRef.current.forEach((enemy) => {
-				// Draw the enemy image assigned to each enemy
 				context.drawImage(
 					enemy.image,
 					enemy.x,
