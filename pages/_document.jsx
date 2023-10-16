80a45cc167d90e794cb10fc02536c55faeaec92f
Ankur Halder <ankur.halder12345@gmail.com>
2023-10-16 15:04:00 +0530

    microsoft clarity added

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,20 +1,33 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
-export default function Document() {
-	return (
-		<Html lang="en">
-			<Head>
-				<meta name="msapplication-config" content="/browserconfig.xml" />
-				<link rel="manifest" href="/manifest.json" />
-				<link
-					href="https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css"
-					rel="stylesheet"
-				/>
-			</Head>
-			<body className="none">
-				<Main />
-				<NextScript />
-			</body>
-		</Html>
-	);
+export default class MyDocument extends Document {
+	render() {
+		return (
+			<Html lang="en">
+				<Head>
+					<meta name="msapplication-config" content="/browserconfig.xml" />
+					<link rel="manifest" href="/manifest.json" />
+					<link
+						href="https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css"
+						rel="stylesheet"
+					/>
+					<script
+						dangerouslySetInnerHTML={{
+							__html: `
+                          (function(c,l,a,r,i,t,y){
+                          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                          })(window, document, "clarity", "script", "ioi3g8s3zl");
+                         `,
+						}}
+					/>
+				</Head>
+				<body className="none">
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
 }
