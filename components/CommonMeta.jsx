09dd911174bae2c3d81aab2d7419628441738632
Ankur Halder <ankur.halder12345@gmail.com>
2024-08-27 08:53:41 +0530

    chore: Update CommonMeta component with additional meta tags

--- components/CommonMeta.jsx
+++ components/CommonMeta.jsx
@@ -1,48 +1,94 @@
 import Head from "next/head";
 export default function CommonMeta() {
-	return (
-		<Head>
-			<meta charSet="UTF-8" />
-			<meta
-				name="viewport"
-				content="width=device-width, initial-scale=1.0, maximum-scale=5.0, user-scalable=yes"
-			/>
-			<meta name="author" content="Ankur Halder" />
-			<meta name="robots" content="index, follow" />
-			<meta property="og:image" content="/icons/favicon-96x96.png" />
-			<meta property="og:type" content="website" />
-			<meta property="og:site_name" content="Space Otaku" />
-			<meta name="twitter:card" content="summary_large_image" />
-			<meta name="twitter:title" content="Space Otaku - Explore the Universe" />
-			<meta
-				name="twitter:description"
-				content="Your ultimate destination for the latest space news, astronaut activities, satellite locations, solar system details, and more. Explore real-time space information with Next.js, Sass, and Dialogflow chatbot."
-			/>
-			<meta name="twitter:image" content="/icons/favicon-96x96.png" />
-			<meta name="twitter:site" content="@SpaceOtakuOfficial" />
-			<link rel="dns-prefetch" href="//www.spaceotaku.online" />
-			<meta name="mobile-web-app-capable" content="yes" />
-			<meta name="apple-mobile-web-app-capable" content="yes" />
-			<meta name="apple-mobile-web-app-status-bar-style" content="black" />
-			<meta name="geo.placename" content="Kolkata, West Bengal, India" />
-			<meta name="geo.position" content="22.476632;88.322844" />
-			<meta name="geo.region" content="IN-WB" />
-			<meta name="language" content="en" />
-			<meta name="target" content="all" />
-			<meta name="HandheldFriendly" content="True" />
-			<meta name="MobileOptimized" content="450" />
-			<meta
-				name="DC.Description"
-				content="Your ultimate destination for space-related information."
-			/>
-			<meta name="pinterest-rich-pin" content="true" />
-			<meta name="rating" content="general" />
-			<meta
-				name="google-site-verification"
-				content="UHzgiXdnV44kTW17GiC7CXXX5NnBxRl4GhXCAfSIXNU"
-			/>
-			<meta name="yandex-verification" content="6bf6f827d7e4d463" />
-			<meta name="seobility" content="bc8ead32fb1b671c727b9e653283b982"></meta>
-		</Head>
-	);
+  return (
+    <Head>
+      <meta charSet="UTF-8" />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0, maximum-scale=5.0, user-scalable=yes"
+      />
+      <meta
+        name="author"
+        content="Ankur Halder , ankur.halder12345@gmail.com"
+      />
+      <meta name="theme-color" content="#000000" />
+      <meta name="subject" content="Ankur Halder portfolio" />
+      <meta name="rating" content="general" />
+      <meta name="referrer" content="no-referrer" />
+      <meta name="distribution" content="global" />
+      <meta name="classification" content="Software Development" />
+      <meta name="designer" content="Ankur Halder" />
+      <meta name="copyright" content="Ankur Halder" />
+      <meta name="robots" content="index, follow" />
+      <meta name="googlebot" content="index, follow" />
+      <meta name="google" content="nositelinkssearchbox" />
+      <meta name="googlebot" content="notranslate" />
+      <meta name="google" content="nopagereadaloud" />
+      <meta property="og:image" content="/icons/favicon-96x96.png" />
+      <meta property="og:type" content="website" />
+      <meta property="og:site_name" content="Space Otaku" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content="Space Otaku - Explore the Universe" />
+      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+      <meta name="mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+      <meta name="geo.placename" content="Kolkata, West Bengal, India" />
+      <meta name="geo.position" content="22.476577;88.32285" />
+      <meta name="geo.region" content="IN-WB" />
+      <meta name="target" content="all" />
+      <meta name="HandheldFriendly" content="True" />
+      <meta name="MobileOptimized" content="450" />
+
+      <meta property="og:type" content="website" />
+      <meta
+        property="og:url"
+        content="https://www.spaceotaku.ankurhalder.in/"
+      />
+      <meta property="og:title" content="Ankur Halder - Software Developer" />
+      <meta
+        property="og:description"
+        content="Passionate software developer proficient in MERN stack, Django, Python, and JavaScript. Active contributor to open-source projects."
+      />
+      <meta
+        property="og:image"
+        content="https://www.ankurhalder.in/Ankur_Halder.png"
+      />
+      <meta
+        property="og:image:alt"
+        content="Ankur Halder - Software Developer"
+      />
+      <meta property="og:site_name" content="Ankur Halder" />
+      <meta name="og:email" content="ankur.halder12345@gmail.com" />
+      <meta name="og:phone_number" content="+919748903490" />
+      <meta name="og:latitude" content="22.476577" />
+      <meta name="og:longitude" content="88.32285" />
+      <meta name="og:street-address" content="!3R Ishan Ghosh Road" />
+      <meta name="og:locality" content="Purba Barisha" />
+      <meta name="og:region" content="West Bengal" />
+      <meta name="og:postal-code" content="700008" />
+      <meta name="og:country-name" content="IND" />
+
+      <meta name="twitter:card" content="Ankur_Halder" />
+      <meta
+        name="twitter:url"
+        content="https://www.spaceotaku.ankurhalder.in/"
+      />
+      <meta name="twitter:title" content="Ankur Halder - Software Developer" />
+      <meta
+        name="twitter:description"
+        content="Passionate software developer proficient in MERN stack, Django, Python, and JavaScript. Active contributor to open-source projects."
+      />
+      <meta
+        name="twitter:image"
+        content="https://www.ankurhalder.in/Ankur_Halder.png"
+      />
+      <meta
+        name="google-site-verification"
+        content="UHzgiXdnV44kTW17GiC7CXXX5NnBxRl4GhXCAfSIXNU"
+      />
+      <meta name="yandex-verification" content="6bf6f827d7e4d463" />
+      <meta name="seobility" content="bc8ead32fb1b671c727b9e653283b982"></meta>
+    </Head>
+  );
 }
